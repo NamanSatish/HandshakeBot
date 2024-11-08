551c220b64eaa6b24a95ea0006b9843e56fb8482
Naman Satish <naman.satish@gmail.com>
2024-11-07 18:51:14 -0800

    start

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa24/class/ee106a-ahs/HandshakeBot/src
+# Build directory: /home/cc/ee106a/fa24/class/ee106a-ahs/HandshakeBot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("hand_pose_tracking")
---
+++ build/hand_pose_tracking/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa24/class/ee106a-ahs/HandshakeBot/src/hand_pose_tracking
+# Build directory: /home/cc/ee106a/fa24/class/ee106a-ahs/HandshakeBot/build/hand_pose_tracking
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
