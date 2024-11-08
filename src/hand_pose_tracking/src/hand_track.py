fe119499388ddef365e4b5ceb4e338b0dafc9f45
Naman Satish <naman.satish@gmail.com>
2024-11-07 18:59:36 -0800

    update

---
+++ src/hand_pose_tracking/src/hand_track.py
@@ -0,0 +1,10 @@
+import rospy
+import time
+import cv2 as cv
+import numpy as np
+import mediapipe as mp
+from geometry_msgs import Point
+
+class HandPoseTracking:
+    def __init__(self, mode=False, hands=1):
+        pass
